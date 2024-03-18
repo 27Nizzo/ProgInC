7057619b6bde3ee17d49fc78ba448e4e9caf56e7
27Nizzo <afonso.martins8282@gmail.com>
2024-03-18 11:32:10 +0000

    50q

--- 1-50/1.c
+++ 1-50/1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*1) Defina um programa que lˆe (usando a fun¸c˜ao scanf uma sequˆencia de n´umeros 
 inteiros terminada com o n´umero 0 e imprime no ecran o maior elemento da sequˆencia.
@@ -103,41 +104,148 @@ int trailingZ(unsigned int n) {
     return zeros; 
 }
 
+/*6) Definir a funçao qDig(unsigned int n) que calcula o numero de digitos necessarios para escrever o inteiro n em base decimal
+Exemplo: qDig(440) deve retornar 3:*/
 
+int qDig(unsigned int n) {
+    int x = 0; 
+    int resto = 0;
+
+    for(int i = 1; resto != n; i *= 10) {
+        resto = n % i;
+        if(resto != n) {
+            x++;
+        }
+    }
+    return x;
+}
+
+/*7) Apresente a definiç~ao da funçao pre-defenida em C char *strcat(char s1[], char s2[]) que concatena a string s2 a s1, retornanado o endereço da primeira*/
+
+char *mystrcat(char s1[], char s2[]) {
+    int p;
+    for (p = 0; s1[p] != '\0'; p++); // Encontra o final de s1
+    
+    int i;
+    for (i = 0; s2[i] != '\0'; i++) { // Concatena s2 em s1
+        s1[p + i] = s2[i];
+    }
+    s1[p + i] = '\0'; // Adiciona o caractere nulo ao final de s1
+    
+    return s1;
+}
+
+/*8) Definiçao da funçao pre-definida strcpy que copia a string source para dest retornando o valor da ultima*/
+
+char *mystrcpy(char *dest, char *source[]) {
+    for(int i = 0; source[i] != '\0'; i++) {
+        dest[i] = '\0';
+    }
+    return dest;
+}
+
+/*9) Definiçao da funçao pre-definida strcmp que compara duas strigs*/
+char *mystrcmp(char s1[], char s2[]) {
+    int i;
+    for(i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++);
+    return s1[i] - s2[i]; 
+}
+
+/*10) Definiçao da funçao pre-definida strstr que determina a posiçao onde a string s2 ocorre na string s1. Retorna NULL caso s2 nao exista em s1 */
+
+int *mystrstr(char s1[], char s2[]) {
+    char *res = NULL; 
+    int i, j;
+    if(s2[0] == '\0') {
+        return s1;
+    }
+    for(i = 0; s1[i] != '\0'; i++) {
+        for(j = 0; s2[j] != '\0' && s1[j] == s1[i+j]; j++);
+        if(s2[j] == '\0')
+        res = s1 + i;
+    }
+    return res;
+    
+}
+
+/*11) Definiçao da funçao strrev que inverte uma string*/
+
+void strrev(char s[]) {
+    int size;
+    char tmp;
+    for(size = 0; s[size] != '\0'; size++);
+    for(int i = 0; i < size/2; i++) {
+        tmp = s[i];
+        s[i] = s[size - i - 1];
+        s[size - i - 1] = tmp;
+    }
+}
+
+/*12) Definiçao de strnoV que retira todas as vogais de uma string  */
+
+void strnoV(char t[]) {
+    int pos = 0; 
+    for(int i = 0; t[i] !='\0'; i++) {
+        if(strchr("aeiouAEIOU", t[i]) == NULL) {
+            t[pos++] = t [i];
+        }
+    }
+    t[pos] = '\0';
+}
+
+/*13) Definiçao da funçao truncW que dado um texto t com varias palavras  e um inteiro n, trunca todas as palavras de forma a terem o maximo n caracteres. */
+
+void truncW(char t[], int n) {
+    int i, adi, pos;
+    for(i = 0, pos = 0, adi = 0; t[i] != '\0'; i++) {
+        if(t[i] == ' ') {
+            adi = 0; 
+            t[pos++] == t[i];
+            adi++;
+        }
+    }
+    t[pos] = '\0';
+}
+
+/*14) Definiçao da funcao charMaisFreq que determina o char mais frequente em uma string*/
+
+char maisMaisFreq(char s[]) {
+    char maisFreq = '0';
+    int i, j, freq = 0, f;
+    for(i = 0; s[i] != '\0'; i++) {
+        f = 0;
+        for(j = 0; s[j] != '\0'; j++) {
+            if(s[j] == s[i]) f++;
+        }
+        if(f > freq) {
+            freq = f;
+            maisFreq = s[i];
+        }
+    }
+    return maisFreq;
+}
+
+/*15) Definiçao da funçao iguaisConsecutivos que dada uma string s calcula o comprimento da maior sub-string com caracteres iguais. */
+
+int iguaisConsecutivos(char s[]) {
+    int i, j;
+    int seq = 0, n;
+
+    for(i = 0, n = 0; s[i] != '\0'; i++, n = 0) {
+        for(j = i; s[j] == s[i] && s[j] != '\0'; j++) n++;
+        if (n > seq) {
+            seq = n;
+        }
+    } 
+    return seq;  
+}
 
 
 int main() {
-
-    int choice = 0;
-
-    printf("Escolhe um exercicio de 1-50: ");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-    case 1:
-        ex1();
-        break;
-
-    case 2: 
-        ex2();
-        break; 
-
-    case 3:
-        ex3();
-        break;
+    char texto[] = "Hello WOrld";
+    strnoV(texto);
+    printf("%s\n", texto);
     
-    case 4:
-        bistUm(5);
-        break;
-    
-    case 5:
-        trailingZ(5);
-        break;
-
-    default:
-        break;
-    }
-
     return 0;
 }
+
--- la/semana1/e/e1.c
+++ la/semana1/e/e1.c
@@ -3,8 +3,11 @@
 int main() {
 // i -> checa se o numero existe ou nao existe, se existir printa "number EXISTS" se não existir o número é inserido
 // d -> dá delete ao numero se existir na hash table e printa "OK", caso contrário printa "NO"
-    int i, d, c;
+// c -> checa se o numero esta na tabela e printa a sua localizaçao, caso contrario printa "NO"
+// p -> printa a tabela omitindo os espaços vazios
 
-printf("Insira o tamanho da hash table: ");
+    int i, d, c;
+    
+    
     return 0;
 }

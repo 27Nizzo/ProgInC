7bae2cd8ddd7dac708d42ee8afe9266a234e249b
27Nizzo <afonso7557@gmail.com>
2024-05-30 21:56:31 +0100

    ficha 5 on going

---
+++ prep/ficha5/exs.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXCHARPARANOME 100
+#define TOTALDEMINITESTES 6
+
+typedef struct aluno 
+{
+    int numero;
+    char nome[MAXCHARPARANOME];
+    int miniT[TOTALDEMINITESTES];
+    float teste;
+} Aluno;
+
+//Ex1:
+int nota(Aluno a){
+    int total = 0;
+    for(int i = 0; i < 6; i++){
+        total += a.miniT[i];
+    }
+    total /= 2;
+    total += a.teste * 0.7;
+    return total;
+}
+
+
+//Ex2:
+int procuraNum(int num, Aluno t[], int N){
+    for(int i = 0; i < N; i++){
+        if(num == t[i].numero){
+            return i;
+        } 
+        else {
+            return -1;
+        }
+    }
+}
+
+//Ex3:
+
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+void ordenaPorNum(Aluno t[], int N){
+   for(int i = 0; i < N - 1; i++){
+    for(int j = i + 1; j < N - i - 1; j++){
+        if(t[j].numero > t[j + 1].numero){
+            swap(&t[j], &t[j + 1]);
+            }   
+        }
+    }
+}
+
+//Ex4:
+
+void criaIndPorNum(Aluno t[], int N, int ind[]){
+    int ind[N];
+
+    for(int i = 0; i < N; i++){
+        ind[i] = i;
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(t[ind[j]].numero < t[ind[i]].numero){
+                swap(ind+i, ind+j);
+            }
+            ind[ind[i]] = i;
+        }
+    }
+}
+
+//Ex5:
+
+void imprimeTurma(int ind[], Aluno t[], int N){
+    int next = 0, i;
+    for(; next < N; next++){
+        for(int i = 0; ind[i] != next; i++);
+            printf("%d %s\n", t[i].numero, t[i].nome);
+        } 
+    }
+
+
+
+int main(){
+    return 0;
+}
